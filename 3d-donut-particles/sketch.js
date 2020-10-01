00a631c68ebee9174b68a606de1cd1449aae8f2f
Thero6th <48805682+Thero6th@users.noreply.github.com>
2020-10-01 16:21:43 -0400

    Add files via upload

---
+++ 3d-donut-particles/sketch.js
@@ -0,0 +1,29 @@
+/*
+
+3D particle system
+4.22.2020
+
+*/
+
+let particleSystem;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    noStroke();
+    particleSystem = new System(createVector(0, 0, 200));
+}
+
+function draw() {
+    background(20);
+
+    // drag to rotate up and down, scroll to zoom in and out
+    orbitControl();
+
+    directionalLight(120, 120, 155, 1, 1, -1);
+    pointLight(355, 355, 355, mouseX - width / 2, mouseY - height / 2, 150);
+
+
+    particleSystem.add();
+    particleSystem.update();
+
+}
